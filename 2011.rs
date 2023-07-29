8d06f43ad6428de613c82142fc0c43934beb503b
ErsCoding <95124076+ErsCoding@users.noreply.github.com>
2023-07-29 18:15:34 +0600

    Create 2011.rs

---
+++ 2011.rs
@@ -0,0 +1,15 @@
+impl Solution {
+    pub fn final_value_after_operations(operations: Vec<String>) -> i32 {
+        let mut res = 0;
+
+        for op in operations {
+            if op == "--X" || op == "X--" {
+                res -= 1;
+            } else{
+                res += 1;
+            }
+        }
+
+        res
+    }
+}
