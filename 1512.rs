2a91d60fb83ebf5fe961c06ad97ca68c33ac747f
ErsCoding <95124076+ErsCoding@users.noreply.github.com>
2023-07-29 18:19:17 +0600

    Create 1512.rs

---
+++ 1512.rs
@@ -0,0 +1,15 @@
+impl Solution {
+    pub fn num_identical_pairs(nums: Vec<i32>) -> i32 {
+        let mut res = 0;
+
+        for i in 0..nums.len() {
+            for j in i+1..nums.len(){
+                if nums[i] == nums[j] {
+                    res += 1;
+                }
+            }
+        }
+
+        res
+    }
+}
